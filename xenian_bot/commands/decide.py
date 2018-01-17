626ca230126f6a0ed96869ab457b9ec190027188
Nick Espig <nickespig@gmail.com>
2018-01-18 00:57:58 +0100

    Create /decide which randomly outputs yes or no

---
+++ xenian_bot/commands/decide.py
@@ -0,0 +1,23 @@
+from random import randint
+
+from telegram import Bot, Update
+
+from . import BaseCommand
+
+__all__ = ['decide']
+
+
+class Decide(BaseCommand):
+    description = 'Yes or No'
+
+    def command(self, bot: Bot, update: Update):
+        """Decide
+
+        Args:
+            bot (:obj:`telegram.bot.Bot`): Telegram Api Bot Object.
+            update (:obj:`telegram.update.Update`): Telegram Api Update Object
+        """
+        update.message.reply_text('Yes' if randint(0, 1) else 'No')
+
+
+decide = Decide()
