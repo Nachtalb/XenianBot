525ab32b122aa6c6ab57e54ec20a7895a394c64d
Nick Espig <nickespig@gmail.com>
2018-01-20 17:46:14 +0100

    Update reverse image search

--- xenian_bot/commands/__init__.py
+++ xenian_bot/commands/__init__.py
@@ -4,7 +4,7 @@ from .urban_dictionary import *
 from .rawl import *
 from .instagram import *
 from .download import *
-# from .reverse_image_search import *
+from .reverse_image_search import *
 # from .danbooru import *
 # from .decide import *
 #
