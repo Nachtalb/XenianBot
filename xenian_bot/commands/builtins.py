8fe895433c30c1f862b614d3b912d0b71bc32f7c
Nick Espig <nickespig@gmail.com>
2018-05-18 20:08:50 +0200

    Use mako for the start message

--- xenian_bot/commands/builtins.py
+++ xenian_bot/commands/builtins.py
@@ -43,12 +43,7 @@ class Builtins(BaseCommand):
             bot (:obj:`telegram.bot.Bot`): Telegram Api Bot Object.
             update (:obj:`telegram.update.Update`): Telegram Api Update Object
         """
-        reply = ('Hello and welcome to me, **@XenianBot**\n\nI am a personal assistant which can do various tasks for '
-                 'you. For example, I can do reverse image searches directly here in Telegram. To see my full '
-                 'capability, use /commands.\nIf you need help plase do not hesitate and contact us with /support.\n\n'
-                 'If you like this bot you can give me a rating here: '
-                 'https://telegram.me/storebot?start=xenianbot')
-        update.message.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
+        update.message.reply_text(render_template('start.html.mako'), parse_mode=ParseMode.HTML)
 
     def commands(self, bot: Bot, update: Update):
         """Generate and show list of available commands
---
+++ xenian_bot/commands/templates/start.html.mako
@@ -0,0 +1,10 @@
+Hello and welcome to me, <b>@XenianBot</b>
+<br>
+<br>
+I am a personal assistant which can do various tasks for you. For example, I can do reverse image searches directly here
+in Telegram. To see my full capability, use /commands.
+<br>
+If you need help plase do not hesitate and contact us with /support.
+<br>
+<br>
+If you like this bot you can give me a rating <a href="https://telegram.me/storebot?start=xenianbot">here</a>
